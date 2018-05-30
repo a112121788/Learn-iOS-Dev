0785be2982a637293c8b9ff31f2be34def211a2b
高帅朋 <gaoshuaipeng@ecloudinfo.com>
2018-05-31 07:40:20 +0800

    day1

---
+++ Day01/DemoPlayground.playground/Contents.swift
@@ -0,0 +1,36 @@
+//: Playground - noun: a place where people can play
+
+//import UIKit
+
+// var 关键字
+var str = "Hello, playground"
+
+//print 方法
+print(str)
+
+print
+(
+    str
+)
+
+//注释1
+
+/*
+ 注释2
+*/
+
+print(str); //这里的分号可以不要
+
+//let a = 1+ 2
+//let a= 1+2// 运算符不能直接跟在变量或常量的后面
+
+
+//接收用户的输入
+let input = readLine()
+
+typealias Int1 = Int
+var height: Int1 = 100
+print(height)
+
+//更多入门教程
+
