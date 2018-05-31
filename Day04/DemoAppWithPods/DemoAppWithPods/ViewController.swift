a84e4596fa72c2a71f96444f266fc41ef5bac0f1
高帅朋 <gaoshuaipeng@ecloudinfo.com>
2018-05-31 15:36:09 +0800

    Material SnapKit  use demo

--- Day04/DemoAppWithPods/DemoAppWithPods/ViewController.swift
+++ Day04/DemoAppWithPods/DemoAppWithPods/ViewController.swift
@@ -2,24 +2,44 @@
 //  ViewController.swift
 //  DemoAppWithPods
 //
-//  Created by 高帅朋 on 2018/5/31.
-//  Copyright © 2018年 高帅朋. All rights reserved.
-//
 
 import UIKit
+import Material
+import SnapKit
+
+struct ButtonLayout {
+    struct Raised {
+        static let width: CGFloat = 150
+        static let height: CGFloat = 44
+        static let offsetY: CGFloat = -75
+    }
+}
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let button = RaisedButton(title: "Button", titleColor: .white)
+        button.pulseColor = .white
+        button.backgroundColor = UIColor(red: 0.98, green: 0.42, blue: 0.61, alpha: 1.00)
+        
+        
+        self.view.addSubview(button)
+
+//        //常规布局
+//        view.layout(button)
+//            .width(ButtonLayout.Raised.width)
+//            .height(ButtonLayout.Raised.height)
+//            .center(offsetY: ButtonLayout.Raised.offsetY)
+        
+        //snap 布局 (也就是自动布局)
+        button.snp.makeConstraints { (make) -> Void in
+            make.width.equalTo(ButtonLayout.Raised.width)
+            make.height.equalTo(ButtonLayout.Raised.height)
+            make.center.equalTo(self.view)
+            make.centerY.equalTo(self.view).offset(ButtonLayout.Raised.offsetY)
+        }
+    
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
