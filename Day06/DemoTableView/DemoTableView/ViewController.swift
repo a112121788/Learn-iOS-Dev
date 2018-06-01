41896fd6ec36b2b05f79a4b66a1dcb6342b71b49
高帅朋 <gaoshuaipeng@ecloudinfo.com>
2018-06-01 15:31:53 +0800

    update 带基础数据的 tableview

--- Day06/DemoTableView/DemoTableView/ViewController.swift
+++ Day06/DemoTableView/DemoTableView/ViewController.swift
@@ -8,16 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    let tableView = UITableView()
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    let tableView = UITableView(frame: UIScreen.main.bounds, style: UITableViewStyle.plain)
+    let table_data = ["1", "2","3"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "首页"
         
-        //table view 
+        //table view
         self.view = tableView
+        //tableView.separatorStyle = UITableViewCellSeparatorStyle.singleLine
+        tableView.tableFooterView = UIView()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
+    }
+    
+    
+    //cell 高度
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    //数量
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return table_data.count
+    }
+    
+    // cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text =  table_data[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("did select \(indexPath.row)")
     }
 }
 
