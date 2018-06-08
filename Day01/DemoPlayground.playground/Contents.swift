316bfb325bcc7a53401dbaa051b5260a91c3fff4
a112121788 <a112121788@163.com>
2018-06-08 14:45:47 +0800

    Revert "no message"
    This reverts commit a069084f979233bbcb885b4c71519b4dcc760534.

--- Day01/DemoPlayground.playground/Contents.swift
+++ Day01/DemoPlayground.playground/Contents.swift
@@ -1,9 +1,43 @@
-import TensorFlow
 
-var x = Tensor<Float>([[1, 2], [3, 4]])
+//: Playground - noun: a place where people can play
 
-for i in 1...5 {
-    x += matmul(x, x)
-}
+//import UIKit
 
-print(x)
+// var 关键字
+var str = "Hello, playground"
+
+//print 方法
+//print(str)
+
+//print
+//(
+//    str
+//)
+
+//注释1
+
+/*
+ 注释2
+*/
+
+//print(str); //这里的分号可以不要
+
+//let a = 1+ 2
+//let a= 1+2// 运算符不能直接跟在变量或常量的后面
+
+
+//接收用户的输入
+let input = readLine()
+
+typealias Int1 = Int
+var height: Int1 = 100
+//print(height)
+
+//更多入门教程
+//https://www.runoob.com/swift/swift-tutorial.html
+
+
+let set :Set = [1,2,3,4,6]
+let set2 = Set(set.map {$0 + 1})
+print(set2)
+
